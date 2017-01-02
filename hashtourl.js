37bc061088aa54a743e75acd614d2b9416965b5b
Smitty <smittyvb@gmail.com>
2017-01-02 22:56:54 +0000

    Tweaks

--- hashtourl.js
+++ hashtourl.js
@@ -5,3 +5,17 @@ addEventListener("hashchange", function () {
   location.pathname = "/prerendered" + 
     location.hash.substr(1, location.hash.length).toLowerCase();
 });
+
+var disqus_url = "http://roadsideattractions.ca/prerendered" + location.pathname.split("/prerendered")[1];
+
+var disqus_config = function () {
+this.page.url = "http://roadsideattractions.ca/prerendered" + location.pathname.split("/prerendered")[1];  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = location.pathname.split("/prerendered")[1]; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = '//roadsides.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
