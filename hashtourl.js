01c8373041ba352f1e335ec7877efcb988eb4167
Smitty <smittyvb@gmail.com>
2017-01-31 21:07:07 +0000

    New file layout system : prerendered -> roadside

--- hashtourl.js
+++ hashtourl.js
@@ -2,7 +2,7 @@ var fixHash = function () {
   if ((location.hash === "#main") || (location.hash === "#/main")) {
     location.pathname = "http://roadsideattractions.ca/";
   }
-  location.pathname = "/prerendered" + 
+  location.pathname = "/roadside" + 
     location.hash.substr(1, location.hash.length).toLowerCase();
 };
 
@@ -10,11 +10,11 @@ addEventListener("hashchange", fixHash);
 addEventListener("load", fixHash);
 
 
-var disqus_url = "http://roadsideattractions.ca/prerendered" + location.pathname.split("/prerendered")[1];
+var disqus_url = "http://roadsideattractions.ca/roadside" + location.pathname.split("/roadside")[1];
 
 var disqus_config = function () {
-this.page.url = "http://roadsideattractions.ca/prerendered" + location.pathname.split("/prerendered")[1];  // Replace PAGE_URL with your page's canonical URL variable
-this.page.identifier = location.pathname.split("/prerendered")[1]; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+this.page.url = "http://roadsideattractions.ca/roadside" + location.pathname.split("/roadside")[1];  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = location.pathname.split("/roadside")[1]; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
 };
 
 (function() { // DON'T EDIT BELOW THIS LINE
