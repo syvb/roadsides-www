3c2e09625f998ca38796b42c1001eb24642749fb
Smitty <smittyvb@gmail.com>
2017-01-31 21:28:24 +0000

    Add sitemap generator
    Added a node.js script to make sitemap files. Note that right now,
    it only supports roadside pages -- static pages are manually
    added to another file. Also note that you need to manaully copy in
    the sitemap file right now, but soon it should be integrated with
    prerendering.

---
+++ sitemap-gen/gen.js
@@ -0,0 +1,36 @@
+var http = require("http");
+var fs = require("fs");
+
+var xml = "";
+xml +=
+  `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+
+function addRoadside(url) {
+  xml += `<url>
+    <loc>http://roadsideattractions.ca/roadside` + url + `</loc>
+    <priority>0.5</priority>
+    <changefreq>yearly</changefreq>
+  </url>
+  `;
+}
+
+http.get("http://new-roadside-stuff-smittyvb.c9users.io:8081/roadsides", function(res) {
+  res.setEncoding('utf8');
+  var rawData = '';
+  res.on('data', (chunk) => rawData += chunk);
+  res.on('end', () => {
+    try {
+      var parsedData = JSON.parse(rawData);
+      parsedData.forEach(function(roadside) {
+        addRoadside(roadside.url);
+      });
+      fs.writeFile("roadsides.xml", xml, "utf-8", function(err) {
+        console.log(err);
+      });
+    }
+    catch (e) {
+      console.log(e.message);
+    }
+  });
+});
