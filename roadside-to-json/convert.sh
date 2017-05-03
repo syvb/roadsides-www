a9dc689dbcf2e726d2b214026115f36c4c98633a
Smitty <smittyvb@gmail.com>
2017-05-03 21:01:06 +0000

    Automaticlly download google sheet
    This automaticlly downloads the Google Sheet with all the roadside
    data, making it easier to render the site. This way, you don't
    need to manually download the Google Sheet.

--- roadside-to-json/convert.sh
+++ roadside-to-json/convert.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Converts  roadsides.csv to csv.json
 
+echo "Downloading Google Sheet..."
+curl https://docs.google.com/spreadsheets/d/1PVn85xw6xt7YQgUYJrMJ_UgTC_XHyvxujfzwc6ff7BM/export?format=csv > io/roadsides.csv
+echo "Turning into JSON"
 csvtojson parse io/roadsides.csv --workerNum=2 --ignoreEmpty=true --trim=true > csvtojson.json
-
+echo "Processing file..."
 node jsonProcess.js
