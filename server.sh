8c6b296e9c939d7c888a70e02215a898476aa0fc
Smitty <smittyvb@gmail.com>
2017-11-14 21:49:41 +0000

    Turn render script into server script.

---
+++ server.sh
@@ -0,0 +1,17 @@
+
+#!/bin/sh
+echo "Starting!"
+kill $(fuser -n tcp 80 2> /dev/null)
+kill $(fuser -n tcp 8081 2> /dev/null)
+kill $(fuser -n tcp 8082 2> /dev/null)
+cd roadside-to-json
+sh convert.sh
+cd ..
+(http-server -p 80 > /dev/null) &  (json-server ../temp-api/roadsides.json --ro --port 8081 > /dev/null) &
+#sleep 3 
+#until (node prerender -a); do
+#    echo "Retrying in 3 seconds."
+#    sleep 3
+#    echo "Retrying."
+#done
+echo "Started servers!"
