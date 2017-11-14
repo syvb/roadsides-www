aa10ddc22da2e6ecf02efa71ff9e5cd9cdc9f5a6
Smittyvb <smittyvb@gmail.com>
2017-11-14 17:28:59 -0500

    Update server.sh

--- server.sh
+++ server.sh
@@ -4,12 +4,13 @@ echo "Starting!"
 kill $(fuser -n tcp 80 2> /dev/null)
 kill $(fuser -n tcp 8081 2> /dev/null)
 kill $(fuser -n tcp 8082 2> /dev/null)
+kill $(fuser -n tcp 8080 2> /dev/null)
 kill $(fuser -n tcp 8443 2> /dev/null)
 
 cd roadside-to-json
 sh convert.sh
 cd ..
-(http-server -p 80 > /dev/null) &  (json-server ../temp-api/roadsides.json --ro --port 8443 > /dev/null) &
+(http-server -p 80 > /dev/null) &  (json-server ../temp-api/roadsides.json --ro --port 8080 > /dev/null) &
 #sleep 3 
 #until (node prerender -a); do
 #    echo "Retrying in 3 seconds."
