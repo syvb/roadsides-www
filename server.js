3c241f3e4cd8d927f0cf286a20e63e38d55e372a
Smittyvb <smittyvb@gmail.com>
2017-11-15 18:27:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ console.log(typeof twitterChange);
 app.use("/", twitterChange);
 
 app.get("/", function (req, res) {
-  if (req.path.startsWith("/roadside") {
+  if (req.path.startsWith("/roadside")) {
     if ((req.path === "/roadside") || (req.path === "/roadside/") || (req.path === "/roadside.html")) {
       req.path = "/roadside/index.html";
     }
