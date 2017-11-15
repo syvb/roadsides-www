0511e118b99e4a30d8a7e4ac2aad439fab8b4c63
Smittyvb <smittyvb@gmail.com>
2017-11-15 18:11:49 -0500

    Update newprerender.js

--- newprerender.js
+++ newprerender.js
@@ -10,7 +10,7 @@ const options = commandLineArgs(optionDefinitions);
 
 puppeteer.launch().then(async browser => {
   const page = await browser.newPage();
-  await page.goto('http://localhost:80');
+  await page.goto('http://localhost:80/#' + options.file);
   const bodyHandle = await page.$('html');
   const html = await page.evaluate(body => body.innerHTML, bodyHandle);
   console.log(html);
