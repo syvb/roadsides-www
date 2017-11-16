f0e3b3e9aee3ab77908b5b235a8b0500674437e2
Smittyvb <smittyvb@gmail.com>
2017-11-16 16:24:37 -0500

    Update newprerender.js

--- newprerender.js
+++ newprerender.js
@@ -1,5 +1,48 @@
-const puppeteer = require('puppeteer');
-var commandLineArgs = require("command-line-args");
+const puppeteer = require("puppeteer");
+const commandLineArgs = require("command-line-args");
+const request = require("request");
+
+const ROADSIDE_LIST = "http://localhost:8081/roadsides";
+
+var renderList = [
+    //static pages
+    "contact",
+    "founder",
+    "pics",
+    "province",
+    //all roadsides page
+    "alphabet",
+    //province pages
+    "alberta",
+    "bc",
+    "manitoba",
+    "newbrunswick",
+    "nfld",
+    "nwt",
+    "novascotia",
+    "nunavut",
+    "ontario",
+    "pei",
+    "quebec",
+    "sask",
+    "yukon",
+    //other
+    "archive",
+    "whatsnew",
+    "tch",
+    "type",
+    "tags",
+    "terms",
+    "media",
+    "extras",
+    "submit"
+    "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"
+];
+
+JSON.parse(request("GET", ROADSIDE_LIST).body.toString())
+  .forEach(function(roadside) {
+    renderList.push(roadside.url.substr(1, roadside.url.length));
+});
 
 const optionDefinitions = [{
   name: 'file',
@@ -8,12 +51,22 @@ const optionDefinitions = [{
 }];
 const options = commandLineArgs(optionDefinitions);
 
-puppeteer.launch().then(async browser => {
-  const page = await browser.newPage();
-  await page.goto('http://localhost:80/#' + options.file);
-  const bodyHandle = await page.$('html');
-  const html = await page.evaluate(body => body.innerHTML, bodyHandle);
-  console.log(html);
-  await bodyHandle.dispose();
-  await browser.close();
-});
+
+//Main loop. This keeps running, rendering everything.
+function renderLoop() {
+  var curRenderList = JSON.parse(JSON.stringify(renderList));
+  while (curRenderList.length > 0) {
+    var roadsideUrl = curRenderList.pop();
+    puppeteer.launch().then(async browser => {
+      const page = await browser.newPage();
+      await page.goto('http://localhost:80/#' + roadsideUrl);
+      const bodyHandle = await page.$('html');
+      const html = await page.evaluate(body => body.innerHTML, bodyHandle);
+      console.log(html);
+      await bodyHandle.dispose();
+      await browser.close();
+    });
+  }
+  setTimeout(renderLoop, 30000);
+}
+renderLoop();
