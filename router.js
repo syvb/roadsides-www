842408fc01440cea4a3c06bd586e13a72a06ea1a
Smittyvb <smittyvb@gmail.com>
2017-11-16 21:00:03 -0500

    Update router.js

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 var Roadsides = window.Roadsides || {};
-Roadsides.API_LOC = "https://" + location.hostname + ":8443/"; //set to location of API server
+Roadsides.API_LOC = "http://" + location.hostname + ":8443/"; //set to location of API server
 /*
 Each item in the routing table is evaluated, in order from top to bottom.
 If one succeeds, then the program stop going through the table.
