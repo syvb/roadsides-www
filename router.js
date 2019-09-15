d1b76b60a8c180980def2fbe30ca22fb22dc3204
Smittyvb <smitty@vanbodegom.ca>
2019-09-15 15:24:52 -0400

    Update router.js

--- router.js
+++ router.js
@@ -111,24 +111,6 @@ addEventListener("load", function() {
           document.title = roadsideData.name;
           setTimeout(function () {
             $(".fotorama").fotorama();
-            
-/**
-*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-
-var disqus_url = "roadsideattractions.ca/prerendered" + roadsideData.url;
-var disqus_config = function () {
-this.page.url = "roadsideattractions.ca/prerendered" + roadsideData.url;  // Replace PAGE_URL with your page's canonical URL variable
-this.page.identifier = roadsideData.url; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-this.page.title = roadsideData.name;
-};
-
-(function() { // DON'T EDIT BELOW THIS LINE
-var d = document, s = d.createElement('script');
-s.src = '//roadsides.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-})();
           }, 100);
           Roadsides.Router.highlightActive();
         });
