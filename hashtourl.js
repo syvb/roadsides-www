28cf44149c2ef4295752b02e10eb09070679cda8
Smittyvb <smitty@vanbodegom.ca>
2019-09-15 15:24:31 -0400

    Update hashtourl.js

--- hashtourl.js
+++ hashtourl.js
@@ -12,21 +12,6 @@ var fixHash = function () {
 addEventListener("hashchange", fixHash);
 addEventListener("load", fixHash);
 
-
-var disqus_url = "http://roadsideattractions.ca/roadside" + location.pathname.split("/roadside")[1];
-
-var disqus_config = function () {
-this.page.url = "http://roadsideattractions.ca/roadside" + location.pathname.split("/roadside")[1];  // Replace PAGE_URL with your page's canonical URL variable
-this.page.identifier = location.pathname.split("/roadside")[1]; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-};
-
-(function() { // DON'T EDIT BELOW THIS LINE
-var d = document, s = d.createElement('script');
-s.src = '//roadsides.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-})();
-
 /*if ((location.pathname === "/roadside/submit") || (location.hash.indexOf("submit") > -1)) {
   setTimeout(function () {document.getElementById('a').innerHTML = '<iframe src="//submit.roadsideattractions.ca/" style="height: 55em;width: 72%;border: none;"></iframe>';}, 0);
 }*/
