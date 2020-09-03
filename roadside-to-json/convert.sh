862e77a09faa6d5fc24637fbebb962af0719fcca
Smitty <smitop2@gmail.com>
2020-09-03 10:24:30 -0400

    Improve convert script

--- roadside-to-json/convert.sh
+++ roadside-to-json/convert.sh
@@ -2,9 +2,9 @@
 # Converts  roadsides.csv to csv.json
 
 echo "Downloading Google Sheet..."
-curl https://docs.google.com/spreadsheets/d/1PVn85xw6xt7YQgUYJrMJ_UgTC_XHyvxujfzwc6ff7BM/export?format=csv > io/roadsides.csv
+curl -L https://docs.google.com/spreadsheets/d/1PVn85xw6xt7YQgUYJrMJ_UgTC_XHyvxujfzwc6ff7BM/export?format=csv > io/roadsides.csv
 echo "Turning into JSON"
-csvtojson parse io/roadsides.csv --workerNum=2 --ignoreEmpty=true --trim=true > csvtojson.json
+csvtojson parse io/roadsides.csv --ignoreEmpty=true --trim=true > csvtojson.json
 echo "Processing file..."
 node jsonProcess.js
 echo "Copying to server..."
